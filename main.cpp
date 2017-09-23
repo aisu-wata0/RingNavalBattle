06e470eca592f1ba8fa80244b5d0dc90960a342d
Aisu Wata <bfs15@inf.ufpr.br>
2017-09-23 18:30:43 -0300

    update, get_ship not ready

--- main.cpp
+++ main.cpp
@@ -5,62 +5,150 @@ enum spot { unk, water, ship_l, ship_u, captain, hit }
 #define MINE 0
 #define ENEMIES 1
 #define fail -1
+#define board_dead -2
+
+class Coord {
+	long y,x;
+}
 
 class Ship {
-	long width;
-	long height;
+	Coord top_left;
+	long height, width;
 	
-	Ship(long width, long height): width(width), height(height){
+	Coord bot_right(){
+		Coord br;
+		br.y = top_left.y + height -1;
+		br.x = top_left.x + width -1;
+		return br;
+	}
+	
+	bool not_exist(Coord sea_max){
+		return (sh.bot_right().y > sea_max.y-1 || sh.bot_right().x > sea_max.x-1
+		|| sh.top_left().x < 0 || sh.top_left().y < 0);
 	}
 }
 
 class Board {
-	long nl,nc;
+	Coord sea_max;
 	int posession;
+	int ship_n, ship_max;
 	vector<spot> sea;
 	
-	Board(long nl, long nc, int posession): sea(nl*nc), posession(posession){
+	Board(Coord sea_max, int posession): sea(sea_max.y*sea_max.x), posession(posession){
 		if(posession == MINE){
-			this->set(water);
+			this->set_board(water);
 		} else {
-			this->set(unk);
+			this->set_board(unk);
 		}
 	}
 	
-	spot& at(long i, long j){
-		return sea.at(i*nc + j);
+	spot& at(long y, long x){
+		return sea.at(y*sea_max.y + x);
+	}
+	spot& at(Coord pos){
+		if( (pos.y*sea_max.x + pos.x) < sea.size() ){
+			return sea.at(pos.y*sea_max.x + pos.x);
+		}else{
+			return unk_def;
+		}
 	}
 	
-	void set(spot sp){
-		for(long i = 0; i < nl; i++){
-			for(long j=0; j < nc; j++){
+	void set_board(spot sp){
+		for(long i = 0; i < sea_max.y; i++){
+			for(long j=0; j < sea_max.y; j++){
 				this->at(i,j) = sp;
 			}
 		}
 	}
 	
-	int set_ship(long y, long x, Ship sh){
-		if(i+sh.height-1 >= nc || j+sh.width-1 >= nl){
+	bool available(spot sp){
+		return sp >= avail_min; // unk or water
+	}
+
+	int set_ship(Ship sh){
+		int ship_id = ship_max;
+		ship_max++;
+		ship_n++;
+		
+		if(sh.not_exists(sea_max)){
 			return fail;
 		}
 		
-		this->at(y,x) = captain;
-		for(long k=y; k <= y+sh.height-1; k++){
-			this->at(k,l) = ship_u;
-		}
-		for(long l=x; l <= x+sh.width-1; l++){
-			this->at(k,l) = ship_l;
-		}
-		for(long k=y+1; k <= y+sh.height-1; k++){
-			for(long l=x+1; l <= x+sh.width-1; l++){
-				this->at(k,l) = ship_l;
+		for(long k = sh.top_left.y; k <= sh.bot_right().y; k++){
+			for(long l = sh.top_left.x; l <= sh.bot_right().x; l++){
+				if(!available(this->at(k,l)){
+					return fail;
+				}
 			}
 		}
+		
+		for(long k = sh.top_left.y; k <= sh.bot_right().y; k++){
+			for(long l = sh.top_left.x; l <= sh.bot_right().x; l++){
+				this->at(k,l) = ship_id;
+			}
+		}
+		return true;
 	}
 	
-	void attackField(long i, long j){
-		if(this->Board.at(i, j) == ship){
-			check_if_ship_destroyed
+	int get_ship(Coord pos, Ship& sh){
+		int ship_hp = 0;
+		Coord it;
+		it = pos;
+		
+		// go left
+		while(this->at(it).id == this->at(pos).id){
+			if(this->at(it).hit == false){
+				ship_hp++;
+			}
+			it.x--;
+		}
+		
+		sh.top_left.x = it.x + 1;
+		
+		// go up and left
+		it.y -= 1;
+		
+		while(this->at(it).id == this->at(pos).id){
+			for(it.x = pos.x; it.x >= sh.top_left.x; it.x--){
+				if(this->at(it).hit == false){
+					ship_hp++;
+				}
+			}
+			it.y--;
+		}
+		sh.top_left.y = it.y + 1;
+		
+		// go right TODO
+		it.y = pos.y;
+		it.x = pos.x + 1;
+		while(this->at(it).id == this->at(pos).id){
+			if(this->at(it).hit == false){
+				ship_hp++;
+			}
+			it.x--;
+		}
+		// go bot right
+		
+		
+		return ship_hp;
+	}
+	
+	int attackField(Coord pos, Ship& sh){
+		int ship_hp;
+		sh.top_left = fail;
+		
+		if(this->Board.at(pos).id <= max_ship_id){
+			this->Board.at(pos).hit = true;
+			
+			if((ship_hp = get_ship(pos, Ship sh)) == 0){
+				ship_n--;
+				if(ship_n == 0){
+					return board_dead;
+				}
+			}
+			return ship_hp;
+		} else {
+			return(fail);
 		}
 	}
 }
