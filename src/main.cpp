c8ac8d164a1043c30d1f82a91865dacbe11337a9
Shadows <kartkork@gmail.com>
2017-09-23 23:53:36 -0300

    Debugging
    Debugging tests

--- src/main.cpp
+++ src/main.cpp
@@ -20,26 +20,68 @@ int main(int argc, char **argv)
 	my_board.print();
 	
 	Ship ship;
+    Ship target;
+    
 	ship.top_left.y = 0;
 	ship.top_left.x = 0;
 	ship.height = 3;
 	ship.width = 1;
 	
 	my_board.set_ship(ship);
-	
+    
 	cout<<"\n\n";
+    
 	my_board.print();
 	
 	ship.top_left.y = 2;
 	ship.top_left.x = 2;
 	ship.height = 1;
 	ship.width = 3;
-	
+    
 	my_board.set_ship(ship);
-	
+
 	cout<<"\n\n";
-	my_board.print();
-	
+
+    my_board.print();
+    
+    Coord att;
+    att.x = 0;
+    att.y = 0;
+
+    my_board.attackField(att, target);
+    att.y++;
+    my_board.print();
+    cout<<"\n\n";
+
+    
+    my_board.attackField(att, target);
+    att.y++;
+    my_board.print();
+    cout<<"\n\n";
+
+
+    my_board.attackField(att, target);
+    my_board.print();
+    cout<<"\n\n";
+
+    //first ship destroyed
+    /*
+    att.x = 2;
+    att.y = 2;
+    
+    my_board.attackField(att, target);
+    att.x++;
+    //my_board.print();
+    
+    my_board.attackField(att, target);
+    att.x++;
+    //my_board.print();
+
+    my_board.attackField(att, target);
+    //my_board.print();
+    //second ship destroyed board dead
+	cout<<"\n\n";*/
+    
 	board_setup(my_board);
 	return 0;
 }
