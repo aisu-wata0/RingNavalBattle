2bd5ce88cb1767f3de105ae323542bc8b7a0aef6
bfs15 <bruno.serbena@gmail.com>
2017-09-24 20:49:07 -0300

    started socket tests

---
+++ src/Network.h
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <memory.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define PORT 9635
+#define PORT_S "9635"
+
+int resolvehelper(const char* hostname, int family, const char* service, sockaddr_storage* pAddr)
+{
+	int result;
+	addrinfo* result_list = NULL;
+	addrinfo hints = {};
+	hints.ai_family = family;
+	hints.ai_socktype = SOCK_DGRAM;
+	result = getaddrinfo(hostname, service, &hints, &result_list);
+	if (result == 0)
+	{
+		memcpy(pAddr, result_list->ai_addr, result_list->ai_addrlen);
+		freeaddrinfo(result_list);
+	}
+
+	return result;
+}
